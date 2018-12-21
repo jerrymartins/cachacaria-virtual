5c98010efe158aecbd4725c9ec6b08cba8270001
Jerry Martins <zeroumbin@gmail.com>
2018-12-21 15:16:18 -0400

    esqueleto - cachaçaria virtual

---
+++ src/test/java/com/cachacaria/virtual/VirtualApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cachacaria.virtual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
