ed97bca080330626cd62142d1ec63537b72e1560
Jerry Martins <zeroumbin@gmail.com>
2018-12-22 11:55:16 -0400

    cria repositórios

---
+++ src/main/java/com/cachacaria/virtual/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.cachacaria.virtual.repository;
+
+import com.cachacaria.virtual.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
