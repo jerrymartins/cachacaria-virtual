7402f05336ab73c8089af52d7140e161fc8fb6b6
Jerry Martins <zeroumbin@gmail.com>
2018-12-22 11:47:41 -0400

    ajusta v2 script

--- src/main/resources/db/migration/V2__testdata.sql
+++ src/main/resources/db/migration/V2__testdata.sql
@@ -1,1 +1,4 @@
-insert into fornecedor(nome, cnpj) values ('Catuaba Ltd', '44588796558');
+insert into fornecedor(nome, cnpj) values ("Catuaba Ltd", "44588796558");
+insert into produto(cod_produto, descricao, fk_fornecedor) values ("ksp89", "Longneek Itaipava", 1);
+
+insert into fornecedor_produto(fornecedor_id, produto_id) values (1,1);
