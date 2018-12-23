6eac1f95405d932505a219532510fa3cb20402c4
Jerry Martins <zeroumbin@gmail.com>
2018-12-23 13:45:11 -0400

    adiciona dados padrões

--- src/main/resources/db/migration/V2__testdata.sql
+++ src/main/resources/db/migration/V2__testdata.sql
@@ -1,2 +1,20 @@
-insert into fornecedores(nome, cnpj) values ("Catuaba Ltd", "44588796558");
-insert into produtos(cod_produto, descricao, fornecedor_id) values ("ksp89", "Longneek Itaipava", 1);
+insert into fornecedores(nome, cnpj) values
+("Catuaba Ltd", "44588796558"),
+("Ambev","45874569874569"),
+("Coca-Cola","45632147896541"),
+("Skol Ltd","45874632145878"),
+("Rio Negro Ltd","45879655574545"),
+("Corona Ltda","88855566998742");
+
+insert into produtos(cod_produto, descricao, fornecedor_id) values
+("ksp89", "Longneek Heineken", 1),
+("lkq58", "Itaipava 600 ml", 2),
+("qas76", "Itaipava LItrão", 2),
+("nbg44", "Brahma 600 ml", 3),
+("qwe43", "Brahma litrão", 3),
+("rty55", "Skol litrão", 4),
+("dew75", "Red Label", 5),
+("lyt79", "Longneeek Priscilla", 1),
+("pok78", "Longneek Itaipava", 2),
+("klo30", "Longneek Corona", 1);
+
