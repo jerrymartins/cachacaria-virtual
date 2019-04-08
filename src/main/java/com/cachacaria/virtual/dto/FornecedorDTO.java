0999c075fa758d0c327992b063dfecd7e44f7696
jerry <zeroumbin@gmail.com>
2019-04-08 14:40:34 -0400

    substitui getter/setter por @data

--- src/main/java/com/cachacaria/virtual/dto/FornecedorDTO.java
+++ src/main/java/com/cachacaria/virtual/dto/FornecedorDTO.java
@@ -1,55 +1,15 @@
 package com.cachacaria.virtual.dto;
 
 import com.cachacaria.virtual.entity.Produto;
+import lombok.Data;
 
 import java.util.Set;
 
+@Data
 public class FornecedorDTO {
     private Long id;
     private String nome;
     private String cnpj;
     private String email;
     private Set<Produto> produtos;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Produto> getProdutos() {
-        return produtos;
-    }
-
-    public void setProdutos(Set<Produto> produtos) {
-        this.produtos = produtos;
-    }
 }
-
-
